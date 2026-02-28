0581e73286ead0c822096a4840a7131038a12915
chxuri <rmamedova2010@gmail.com>
2026-02-28 14:16:41 -0600

    made guest/member sections for login

--- projects/HFTW 2026/myapp/server.js
+++ projects/HFTW 2026/myapp/server.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const app = express(); // Create an Express application instance
 const PORT = 3000; // Define the port number
 
+
 // Define a route for GET requests to the root URL (/)
 //app.get('/', (req, res) => {
   //res.send('index.html'); // Send a response to the client
@@ -12,8 +13,11 @@ app.use(express.urlencoded({ extended: true }))
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
 });
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
+});
 
-app.post("/login", (req, res) => {
+app.post("/signup1", (req, res) => {
   const { username, password } = req.body;
 
   fs.readFile("public/users.txt", "utf8", (err, data) => {
@@ -28,13 +32,41 @@ app.post("/login", (req, res) => {
     }
     console.log(data)
     console.log(users)
+    if (users[username]) {
+      res.send("User already exists");
+    } else {
+      fs.appendFile("public/users.txt", `${username}:${password}\n`, () => {
+        res.send("Signup successful! <a href='/login.html'>Login</a>");
+      });
+    }
+  });
+});
+
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  fs.readFile("public/users.txt", "utf8", (err, data) => {
+    let users = {};
+    //console.log(err)
+
+    if (data) {
+      data.split("\n").forEach(line => {
+        const [u, p] = line.split(":");
+        if (u) users[u] = p;
+      });
+    }
+    console.log(data)
+    console.log(users)
     if (users[username] === password) {
-      res.send("Welcome, " + username);
+      //res.send("Welcome, " + username);
+      res.redirect("index.html?username=${encodeURIComponent(username)}")
     } else {
       res.send("Invalid username or password");
     }
   });
 });
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server and listen on the specified port
